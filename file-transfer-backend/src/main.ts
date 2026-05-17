2e051d961bd1cd126bb40b143880af397c0bdaf2
MohamedArshath10 <arshath.m2003@gmail.com>
2026-05-17 20:06:39 +0530

    Altered cors for production

--- file-transfer-backend/src/main.ts
+++ file-transfer-backend/src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      'http://localhost:5173',
+      process.env.FRONTEND_URL || 'https://file-transfer-system-1-nmvu.onrender.com/',
+    ],
     credentials: true,
   });
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
 bootstrap();
