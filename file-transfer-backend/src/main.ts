991392861ffa8585d3bf7ef141b9bfac57a86ab6
MohamedArshath10 <arshath.m2003@gmail.com>
2026-05-17 20:19:14 +0530

    Worked on cors

--- file-transfer-backend/src/main.ts
+++ file-transfer-backend/src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      process.env.FRONTEND_URL || 'https://file-transfer-system-1-nmvu.onrender.com/',
-    ],
+    origin: true, 
     credentials: true,
   });
+
   await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
 bootstrap();
